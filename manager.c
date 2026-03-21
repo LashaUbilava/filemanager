d0474745dbd0f59249d19c0099c8af8d91f7d29f
Лаша <lashaubilava46@gmail.com>
2026-03-21 21:21:33 +0300

    добавил логику добавления файла

--- manager.c
+++ manager.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
 
 FILE *file;
+
+
+void create_file(char *path){
+    char name_file[50];
+    char final_path[256];
+    char dir_path[250];
+    char content[256];
+    char* first_path = "file";
+    if (mkdir(first_path, 0775) !=0 && errno != EEXIST){    
+        perror("Error");
+        return;
+    }
+    printf("Выберите путь, куда вы хотите записать файл(Пример ввода путь/путь): ");
+    scanf("%255s", path);
+    snprintf(dir_path, sizeof(dir_path), "%s/%s", first_path, path);
+    if (mkdir(dir_path, 0775) != 0 && errno != EEXIST){    
+        perror("Error");
+        return;
+    }
+    printf("Введите название текстового файла: ");
+    scanf("%49s", name_file);
+    snprintf(final_path, sizeof(final_path), "%s/%s/%s", first_path, path, name_file);
+    file = fopen(final_path, "w");
+    printf("Введите любой текст: ");
+    scanf("%255s", content);
+    fprintf(file, "%s", content);
+    fclose(file);
+    return;
+}
 
 char* hash_function(char password[]) {
     static char stri[16];
@@ -39,7 +72,7 @@ int autetification (char login[], char password[]){
         return 0;
     } else{
         printf("Не удалось войти. Попробуйте авторизоваться\n");
-        return 0;
+        return 1;
     }
     return  0;
 }
@@ -63,11 +96,13 @@ int autorization(char login[], char password[]){
 int main(void){
     char login[50];
     char password[50];
-    int choice;
+    int choice1;
+    int choice2;
+    char path[256];
     printf("Hello! 1. Log in or 2. Autorize\n");
     printf("1. Log in\n");
     printf("2. Autorize\n");
-    scanf("%d", &choice);
+    scanf("%d", &choice1);
 
     printf("Login: ");
     scanf("%49s", login);
@@ -75,13 +110,18 @@ int main(void){
     printf("Password: ");
     scanf("%49s", password);
 
-    if (choice == 1){
-        autetification(login, password);
-        return 0;
+    if (choice1 == 1){
+        if (autetification(login, password) != 0){
+            exit(-1);
+        }
     }
-    else if (choice == 2){
+    else if (choice1 == 2){
        autorization(login,  password);
-        return 0;
+    }
+    printf("Выберите действие: 1. Записать текстовый файл\n");
+    scanf("%d", &choice2);
+    if (choice2 == 1){
+        create_file(path);
     }
     return 0;
 }
